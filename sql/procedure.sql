91639b3c3a827a8199f0e2e715aaadd4935be4f6
C-atarina <catarina.silvaroriz@viacesi.fr>
2021-03-19 22:44:48 +0100

    MaJ procédure insert pilote

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -4,7 +4,6 @@ USE GroupeCat_Test
 
 DELIMITER |
 
--- Creation Etudiant
 DROP PROCEDURE IF EXISTS Creation_Etudiant |
 CREATE PROCEDURE Creation_Etudiant(IN nom VARCHAR(50), IN prenom VARCHAR(50), IN email VARCHAR(50), IN pwd VARCHAR(50), IN pilote INT, IN promotion INT, IN specialite INT)
 BEGIN
@@ -28,8 +27,6 @@ BEGIN
 END |
 -- CALL Creation_Etudiant('rttz', 'zret', 'zretze@zret', 'zert', 2, 1, 2);
 
-
--- Affichage Etudiant
 DROP PROCEDURE IF EXISTS Affichage_Etudiant |
 CREATE PROCEDURE Affichage_Etudiant(IN page INT)
 BEGIN
@@ -46,19 +43,19 @@ BEGIN
   INNER JOIN Droit d
   ON Users.Id_Users = d.Id_Users
   WHERE d.Id_Statut = 4
-  ORDER BY Users.Id_Users
+  -- ORDER BY USERS.Id_Users
   LIMIT page, 10;
 END |
 
+-- Creation_Pilote
 
--- Creation Pilote
 DROP PROCEDURE IF EXISTS Creation_Pilote |
 CREATE PROCEDURE Creation_Pilote(IN i_nom VARCHAR(50),IN i_prenom VARCHAR(50),IN i_mail VARCHAR(50),IN i_passwd VARCHAR(50),IN i_id_centre VARCHAR(50))
 BEGIN
 DECLARE id_use INT;
 
 INSERT INTO Users(Nom,Prenom,Email,Passwd,Id_Centre)
-VALUES(i_nom,i_prenom,i_mail,i_passwd,i_id_centre);
+VALUES(LOWER(i_nom),LOWER(i_prenom),i_mail,i_passwd,i_id_centre);
 
 SELECT LAST_INSERT_ID() INTO id_use;
 
@@ -68,8 +65,8 @@ VALUES(id_use,2,1,1,0,1,1,1);
 
 END |
 
+--Affichage_Pilote
 
--- Affichage Pilote
 DROP PROCEDURE IF EXISTS Affichage_Pilote |
 CREATE PROCEDURE Affichage_Pilotes(IN page INT)
 BEGIN
