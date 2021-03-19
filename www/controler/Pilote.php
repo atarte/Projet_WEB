c469668a1d6b835160e325d7f4a8e95889989dea
C-atarina <catarina.silvaroriz@viacesi.fr>
2021-03-19 14:58:10 +0100

    controler pilote

--- www/controler/Pilote.php
+++ www/controler/Pilote.php
@@ -1,6 +1,12 @@
 <?php
 
 class Pilote extends Controler {
+
+  public function __construct() {
+      session_start();
+      $this->loadModel("Pilote_Model");
+  }
+
     public function index() {
 
       $this->loadModel("Pilote_Model");
