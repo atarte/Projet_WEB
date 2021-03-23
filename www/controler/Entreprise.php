bed2f9bb5dc4739fce7c774056b59e22c22d0401
Lau-cesi <73819470+Lau-cesi@users.noreply.github.com>
2021-03-23 16:49:28 +0100

    Update Entreprise.php

--- www/controler/Entreprise.php
+++ www/controler/Entreprise.php
@@ -1,9 +1,71 @@
 <?php
 
 class Entreprise extends Controler {
-    public function index() {
+    public $entreprise;
+    public $secteur;
+
+    public $err;
+    public $p;
+
+
+    public function __construct() {
+        session_start();
         $this->loadModel("Entreprise_Model");
 
-        
+        $this->p = 1;
+
+        $this->secteur = $this->Entreprise_Model->getSecteur();
+    }
+
+
+    public function index() {
+        $this->affichage();
+    }
+
+
+    public function affichage() {
+        $pageLimit = 10 * ($this->p -1);
+
+        $this->entreprise = $this->Entreprise_Model->displayEntreprise($pageLimit);
+
+        require_once(ROOT.'view/Entreprise_View.php');
+
+    }
+
+
+    public function page(int $page = 1) {
+        $this->p = $page;
+
+        $this->affichage();
+    }
+
+
+    public function creation() {
+        $this->err = $this->Entreprise_Model->addEntreprise();
+
+        $this->affichage();
+    }
+
+
+    public function suppression(int $id) {
+        $this->err = $this->Entreprise_Model->deleteEntreprise($id);
+
+        $this->affichage();
+    }
+
+
+    public function modification(int $id) {
+        $this->err = $this->Entreprise_Model->updateEntreprise($id);
+
+        $this->affichage();
+    }
+
+
+    public function recherche() {
+        $this->entreprise = $this->Entreprise_Model->search();
+
+        $this->p = 0;
+
+        require_once(ROOT.'view/Entreprise_View.php');
     }
 }
