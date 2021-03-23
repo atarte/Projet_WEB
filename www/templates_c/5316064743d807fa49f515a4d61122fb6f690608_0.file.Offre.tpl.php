e20055e3b0fe46d2d0e7ffffc04f87c6b9f56020
C-atarina <catarina.silvaroriz@viacesi.fr>
2021-03-24 00:34:01 +0100

    mise à jour

--- www/templates_c/5316064743d807fa49f515a4d61122fb6f690608_0.file.Offre.tpl.php
+++ www/templates_c/5316064743d807fa49f515a4d61122fb6f690608_0.file.Offre.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-23 23:06:25
+/* Smarty version 3.1.39, created on 2021-03-23 23:30:25
   from 'C:\Users\catar\Documents\CESI\A2\4 - Web\Projet\Projet_WEB\www\view\layout\Offre.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605a74712b42f6_26378658',
+  'unifunc' => 'content_605a7a112679f0_31055922',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5316064743d807fa49f515a4d61122fb6f690608' => 
     array (
       0 => 'C:\\Users\\catar\\Documents\\CESI\\A2\\4 - Web\\Projet\\Projet_WEB\\www\\view\\layout\\Offre.tpl',
-      1 => 1616540594,
+      1 => 1616542204,
       2 => 'file',
     ),
   ),
@@ -22,158 +22,171 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:./common/footer.tpl' => 1,
   ),
 ),false)) {
-function content_605a74712b42f6_26378658 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605a7a112679f0_31055922 (Smarty_Internal_Template $_smarty_tpl) {
 ob_start();
 echo $_smarty_tpl->tpl_vars['title']->value;
 $_prefixVariable1 = ob_get_clean();
 $_smarty_tpl->_subTemplateRender("file:./common/header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('title'=>$_prefixVariable1), 0, false);
 ?>
 
+<?php echo $_smarty_tpl->tpl_vars['role']->value;?>
+
+<link rel="stylesheet" href="http://static.projet.com/css/Offre.css">
+
 <body>
-    <?php echo $_smarty_tpl->tpl_vars['role']->value;?>
 
-    <a href="/Accueil"><button>Retour</button></a>
-    <br><br>
-    <form action="/Offre/creation" method="post" id="formulaire">
+<main class="container">
+    <nav>
+        <a href="/Accueil"><button>Retour</button></a>
+    </nav>
 
-        Nom : <br>
-        <input id="nom" type="text" name="nom" required>
+    <div class="row justify-content-between">
+        <article class="col">
 
-        <br>
-        <br>
-        
-        Type de Promotion : <br>
-        <select id="specialite" name="specialite" required>
-            <option value="">--Choississez une spécialité--</option>
-            <?php echo $_smarty_tpl->tpl_vars['Type']->value;?>
+            <!-- création étudiant -->
+            <fieldset class="field">
+                <legend id="legend_form">Création d'une offre de Stage</legend>
 
-        </select>
+                <form id="formulaire" class="form" action="/Offre/creation" method="post">
+                    <div class="container">
+                        <div class="row justify-content-center p-1">
+                            <!-- nom : -->
+                            <input id="nom" type="text" name="nom" placeholder="Nom" required>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Type de Promotion : <br> -->
+                            <select id="specialite" name="specialite" required>
+                                <option value="">Choississez une spécialité</option>
+                                <?php echo $_smarty_tpl->tpl_vars['Type']->value;?>
 
-        <br>
-        <br>
+                            </select>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Entreprise : <br> -->
+                            <select id="entreprise" name="entreprise" required>
+                                <option value="">Choississez une entreprise</option>
+                                <?php echo $_smarty_tpl->tpl_vars['Entreprise']->value;?>
 
-        Entreprise : <br>
-        <select id="entreprise" name="entreprise" required>
-            <option value="">--Choississez une entreprise--</option>
-            <?php echo $_smarty_tpl->tpl_vars['Entreprise']->value;?>
+                            </select>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Ville : <br> -->
+                            <select id="ville" name="ville">
+                                <option value="">Choisiez une ville</option>
+                                <?php echo $_smarty_tpl->tpl_vars['Ville']->value;?>
 
-        </select>
+                            </select>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Email : <br> -->
+                            <input id="email" type="email" name="email" placeholder="Email" required>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Durée : <br> -->
+                            <input id="duree" type="text" name="duree" placeholder="Durée" required>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Compétences : <br> -->
+                            <input id="competences" type="text" name="competences" placeholder="Compétence(s)" required>
+                            <!-- <button onclick="add()">+</button> -->
+                            <!-- <div id="plus_comp"></div> -->
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Nombre de place(s) : <br> -->
+                            <input id="nb_place" type="text" name="nb_place" placeholder="Nombre de place(s)" required>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Rémunération : <br> -->
+                            <input id="remuneration" type="text" name="remuneration" placeholder="Rémunération" required>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <!-- Date : <br> -->
+                            <input id="date" type="date" name="date" placeholder="Date" required>
+                        </div>
+                        <div class="row justify-content-center p-1">
+                            <input type="submit" value="Créer" id="submit">
+                        </div>
+                </form>
+                        <div class="row justify-content-center p-1">
+                            <span id="annuler">
+                            </span>
+                        </div>
+                    </div>
+            </fieldset>
+        </article>
 
-        <br>
-        <br>
+        <article class="col">
+            <fieldset class="field">
+                <legend>Liste des Stages</legend>
 
-        Ville : <br>
-        <select id="ville" name="ville">
-            <option value="">--Choisiez une ville--</option>
-            <?php echo $_smarty_tpl->tpl_vars['Ville']->value;?>
+                <div class="">
+                    <!-- Barre de Recherche des Offres -->
+                    <form id="recherche" action="/Offre/recherche" method="post">
 
-        </select>
+                        <!-- Entreprise : -->
+                        <select id="r_entreprise" name="entreprise" >
+                            <option value="">--Choississez une entreprise--</option>
+                            <?php echo $_smarty_tpl->tpl_vars['Entreprise']->value;?>
 
-        <br>
-        <br>
+                        </select>
 
-        Email : <br>
-        <input id="email" type="email" name="email" required>
+                        <!-- Ville : -->
+                        <select id="r_ville" name="ville">
+                            <option value="">--Choisiez une ville--</option>
+                            <?php echo $_smarty_tpl->tpl_vars['Ville']->value;?>
 
-        <br>
-        <br>
+                        </select>
 
-        Durée : <br>
-        <input id="duree" type="text" name="duree" required>
+                        <!-- Compétences : -->
+                        <select id="r_competence" name="competence">
+                            <option value="">Choisiez le(s) Compétence(s)</option>
+                            <?php echo $_smarty_tpl->tpl_vars['Competence']->value;?>
 
-        <br>
-        <br>
+                        </select>
 
-        Compétences : <br>
-        <input id="competences" type="text" name="competences" required>
-        <button onclick="add()">+</button>
-        <div id="plus_comp"></div>
+                        <!-- Durée : -->
+                        <select id="r_durer" name="durer">
+                            <option value="">Choisiez la Durée de votre stage</option>
+                            <?php echo $_smarty_tpl->tpl_vars['Durer']->value;?>
 
-        <br>
-        <br>
+                        </select>
 
-        Nombre de place(s) : <br>
-        <input id="nb_place" type="text" name="nb_place" required>
+                        <!-- Rémunération : -->
+                        <select id="r_remuneration" name="remuneration">
+                            <option value="">Choisiez une rémunération</option>
+                            <option value="0"> Non rémunéré </option>
+                            <option value="100"> > 100 </option>
+                            <option value="300"> > 300 </option>
+                            <option value="500"> > 500 </option>
+                            <option value="700"> > 700 </option>
+                            <option value="1000"> > 1000 </option>
+                        </select>
 
-        <br>
-        <br>
+                        <input id="r_submit" type="submit" value="Rechercher">
 
-        Rémunération : <br>
-        <input id="remuneration" type="text" name="remuneration" required>
+                        <span id="close">
+                            <?php echo (($tmp = @$_smarty_tpl->tpl_vars['close']->value)===null||$tmp==='' ? '' : $tmp);?>
 
-        <br>
-        <br>
+                        </span>
+                    </form>
+                </div>
 
-        Date : <br>
-        <input id="date" type="date" name="date" required>
+                <!-- Affichage : -->
+                <div class="page">
+                    <?php echo (($tmp = @$_smarty_tpl->tpl_vars['pagination']->value)===null||$tmp==='' ? '' : $tmp);?>
 
-        <br>
-        <br>
+                </div>
 
-        <input type="submit" value="Créer" id="submit">
-        <br>
+                <div class="">
+                    <?php echo $_smarty_tpl->tpl_vars['Offre']->value;?>
 
-      </form>
+                </div>
+            </fieldset>
+        </article>
+    </div>
+</main>
 
-      <div id="annuler">
-      </div>
-
-      <br>
-
-      Barre de Recherche des Offres
-      <form id="recherche" action="/Offre/recherche" method="post">
-          Entreprise :
-          <select id="r_entreprise" name="entreprise" >
-              <option value="">--Choississez une entreprise--</option>
-              <?php echo $_smarty_tpl->tpl_vars['Entreprise']->value;?>
-
-          </select>
-
-          Ville :
-          <select id="r_ville" name="ville">
-              <option value="">--Choisiez une ville--</option>
-              <?php echo $_smarty_tpl->tpl_vars['Ville']->value;?>
-
-          </select>
-
-          Compétences :
-          <select id="r_competence" name="competence">
-              <option value="">--Choisiez le(s) Compétence(s)--</option>
-              <?php echo $_smarty_tpl->tpl_vars['Competence']->value;?>
-
-          </select>
-
-          Durée :
-          <select id="r_durer" name="durer">
-              <option value="">--Choisiez la Durée de votre stage--</option>
-              <?php echo $_smarty_tpl->tpl_vars['Durer']->value;?>
-
-          </select>
-
-          Rémunération :
-          <select id="r_remuneration" name="remuneration">
-              <option value="">--Choisiez le(s) Compétence(s)--</option>
-              <option value="0"> Non rémunéré </option>
-              <option value="100"> > 100 </option>
-              <option value="300"> > 300 </option>
-              <option value="500"> > 500 </option>
-              <option value="700"> > 700 </option>
-              <option value="1000"> > 1000 </option>
-          </select>
-
-          <input id="r_submit" type="submit" value="Rechercher">
-      </form>
-      <a href="/Offre"><button>X</button></a><br><br>
-
-        <?php echo $_smarty_tpl->tpl_vars['Offre']->value;?>
-
-
-      <div>
-          <?php echo (($tmp = @$_smarty_tpl->tpl_vars['pagination']->value)===null||$tmp==='' ? '' : $tmp);?>
-
-      </div>
-
-      <?php echo '<script'; ?>
+    <?php echo '<script'; ?>
  src="/public/js/Offre.js" charset="utf-8"><?php echo '</script'; ?>
 >
 </body>
--- www/view/Offre_View.php
+++ www/view/Offre_View.php
@@ -7,128 +7,6 @@ $smarty->assign('title', 'Gestion Offre');
 $html = '<input type="hidden" id="role" value="'.$_SESSION['role'].'">';
 $smarty->assign('role', $html);
 
-// $html = '';
-// while ($row = $this->wish->fetch()) {
-//     $html = $html.'<input type="hidden" id="stage_id" value="'.$row['id_stage'].'">';
-//     $html = $html.'<input type="hidden" id="user_id" value="'.$row['id_user'].'">';
-//     $html = $html.'<input type="hidden" id="souhait_id" value="'.$row['souhait'].'">';
-//     $html = $html.'<input type="hidden" id="id" value="'.$_SESSION['id'].'">';
-// }
-// $smarty->assign('Candidature', $html);
-
-// if ($_SESSION['role'] == "1" || $_SESSION['role'] == "2" || ($_SESSION['role'] == "3" && $_SESSION['deleg']['offre'] == "1")) {
-//     $html = '';
-//     while ($row = $this->type_promo->fetch()) {
-//         $html = $html.'<option value="'.$row['id'].'">'.$row['spe'].'</option>';
-//     }
-//
-//     $smarty->assign('Type', $html);
-//
-//
-//     $html = '';
-//     while ($row = $this->ville->fetch()) {
-//         $html = $html.'<option value="'.$row['id'].'">'.$row['ville'].'</option>';
-//     }
-//
-//     $smarty->assign('Ville', $html);
-//
-//
-//     $html = '';
-//     while ($row = $this->entreprise->fetch()) {
-//         $html = $html.'<option value="'.$row['id'].'">'.$row['entreprise'].'</option>';
-//     }
-//
-//     $smarty->assign('Entreprise', $html);
-//
-//
-//     $html = '';
-//     while ($row = $this->competence->fetch()) {
-//         $html = $html.'<option value="'.$row['competence'].'">'.$row['competence'].'</option>';
-//     }
-//
-//     $smarty->assign('Competence', $html);
-//
-//     $html = '';
-//     while ($row = $this->durer->fetch()) {
-//         $html = $html.'<option value="'.$row['durer'].'">'.$row['durer'].'</option>';
-//     }
-//
-//     $smarty->assign('Durer', $html);
-//
-//
-//     $html = '';
-//     $i = 0;
-//
-//     while ($row = $this->offre->fetch()) {
-//         $html = $html.'<div id="'.$row["id"].'">';
-//
-//         $html = $html.' Nom : <span id="nom_'.$row['id'].'">'.$row['nom'].'</span><br>';
-//
-//         $html = $html.' Entreprise : <span id="entreprise_'.$row['id'].'">'.$row['entreprise'].'</span><br>';
-//
-//         $html = $html.' Ville : <span id="ville_'.$row['id'].'">'.$row['ville'].'</span><br>';
-//
-//         $html = $html.' Durée : <span id="duree_'.$row['id'].'">'.$row['stage'].'</span><br>';
-//
-//         $html = $html.' Spécialité : <span id="specialite_'.$row['id'].'">'.$row['specialite'].'</span><br>';
-//
-//         $html = $html.'Compétence(s) : <span id="competences_'.$row['id'].'">'.$row['competence'].'</span><br>';
-//
-//         $html = $html.' Nombres de place(s) : <span id="nb_'.$row['id'].'">'.$row['nb_place'].'</span><br>';
-//
-//         $html = $html.' Rémunération (mensuelle) : <span id="remuneration_'.$row['id'].'">'.$row['remuneration'].' €</span><br>';
-//
-//         $html = $html.' Date de dépôt : <span id="date_'.$row['id'].'">'.$row['date_offre'].'</span><br>';
-//
-//         $html = $html.' Email : <span id="email_'.$row['id'].'">'.$row['email'].'</span><br>';
-//
-//         $html = $html.'<button onclick=confirmation('.$row['id'].')>Supprimer</button>';
-//
-//         $html = $html.'<button onclick=modification('.$row['id'].')>Modifier</button><br><br>';
-//
-//         $html = $html.'<br></div>';
-//
-//     }
-//
-//     $smarty->assign('Offre',$html);
-//
-//
-//
-//     if (isset($this->err)) {
-//         if ($$this->err == 1) {
-//             $smarty->assign('erreur', 'Email déjà utilisé pour un autre compte');
-//         }
-//         else {
-//         }
-//     }
-//
-//
-//
-//
-//     $page  = $this->p;
-//     $pageBack = $page -1;
-//     $pageForward = $page +1;
-//
-//
-//     $html = '<span ';
-//     if ($page == 1) {
-//         $html = $html.'hidden';
-//     }
-//     $html = $html.'><a href="/Offre/page/'.$pageBack.'"> << </a></span>';
-//
-//     $html = $html.'<span>page '.$page.'</span>';
-//
-//     $html = $html.'<span ';
-//     if ($i < 10) {
-//         $html = $html.'hidden';
-//     }
-//     $html = $html.'><a href="/Offre/page/'.$pageForward.'"> >> </a></span>';
-//
-//     if ($this->p != 0) {
-//         $smarty->assign('pagination', $html);
-//     }
-// }
-//if ($_SESSION['role'] == "4") {
 
 // Affichage des Spécialité
 $html = '';
@@ -204,25 +82,9 @@ while ($row = $this->offre->fetch()) {
 
     $html = $html.' Email : <span id="email_'.$row['id'].'">'.$row['email'].'</span><br>';
 
-
-
-
-        // echo sizeof($cand);
-        //
-        //
-        // for ($i=0; $i < count($cand); $i++) {
-        //
-        //     echo 'prout';
-        //     $jsp = '';
-        //
-        //     if ($cand[$i]['id_user'] == $_SESSION['id'] && $cand[$i]['id_stage'] == $row['id'] && !empty($cand[$i]['souhait'])) {
-        //         $jsp ='<button id="btn_wish">Déwishlister</button><br><br>';
-        //         break;
-        //     }
-        //     else {
-        //     }
-        // }
     $jsp = '';
+
+    $jsp2 = '';
 
     if ($_SESSION['role'] == 4) {
 
@@ -236,14 +98,14 @@ while ($row = $this->offre->fetch()) {
 
                 if ($cand[$i]['id_stage'] == $row['id'] && !empty($cand[$i]['souhait'])) {
 
-                    $html = $html.'<button onclick=postuler('.$row['id'].')>Dépostuler</button>';
+                    $jsp2 = $jsp2.'<button onclick=postuler('.$row['id'].')>Dépostuler</button>';
 
                     $jsp ='<a href="/Offre/deleteWishlist/'.$row['id'].'"><button>Déwishlister</button></a><br><br>';
                     break;
                 }
                 else {
 
-                    $html = $html.'<button onclick=postuler('.$row['id'].')>Postuler</button>';
+                    $jsp2 = $jsp2.'<button onclick=postuler('.$row['id'].')>Postuler</button>';
 
                     $jsp = '<a href="/Offre/wishList/'.$row['id'].'"><button>WishList</button></a><br><br>';
                 }
@@ -296,9 +158,6 @@ $html = $html.'><a href="/Offre/page/'.$pageForward.'"> >> </a></span>';
 if ($this->p != 0) {
     $smarty->assign('pagination', $html);
 }
-
-
-//}
 
 
 
