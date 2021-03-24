127803d068937226fad2a6f5e4e553402feb920a
C-atarina <catarina.silvaroriz@viacesi.fr>
2021-03-24 15:17:09 +0100

    procedure

--- www/templates_c/6fea429b4f392f936b8ea4c461fbbc6e99c457ec_0.file.Candidature.tpl.php
+++ www/templates_c/6fea429b4f392f936b8ea4c461fbbc6e99c457ec_0.file.Candidature.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-03-24 13:55:14
+/* Smarty version 3.1.39, created on 2021-03-24 14:14:49
   from 'C:\Users\catar\Documents\CESI\A2\4 - Web\Projet\Projet_WEB\www\view\layout\Candidature.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_605b44c21585b8_17006083',
+  'unifunc' => 'content_605b4959444a38_68595720',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '6fea429b4f392f936b8ea4c461fbbc6e99c457ec' => 
     array (
       0 => 'C:\\Users\\catar\\Documents\\CESI\\A2\\4 - Web\\Projet\\Projet_WEB\\www\\view\\layout\\Candidature.tpl',
-      1 => 1616594092,
+      1 => 1616595232,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:./common/footer.tpl' => 1,
   ),
 ),false)) {
-function content_605b44c21585b8_17006083 (Smarty_Internal_Template $_smarty_tpl) {
+function content_605b4959444a38_68595720 (Smarty_Internal_Template $_smarty_tpl) {
 ob_start();
 echo $_smarty_tpl->tpl_vars['title']->value;
 $_prefixVariable1 = ob_get_clean();
@@ -47,8 +47,10 @@ $_smarty_tpl->_subTemplateRender("file:./common/header.tpl", $_smarty_tpl->cache
                 <legend id="legend_form">Liste des candidatures</legend>
 
                 <!-- affichages des stages postulé -->
+                <div class="">
+                    <?php echo $_smarty_tpl->tpl_vars['candidature']->value;?>
 
-
+                </div>
             </fieldset>
         </article>
     </div>
